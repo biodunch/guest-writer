9ba814eebc5abc7c085477993b42013f1d72a8b0
Biodun Chris <biodunch@gmail.com>
2018-04-19 17:32:39 +0100

    complete building birthdates api with restify

--- src/docker.js
+++ src/docker.js
@@ -1,13 +1,20 @@
-const {spawn} = require('child_process');
-const ls = spawn('docker', [
-    'run', '-p', '4000:4000', '--name', 'blog', '-d', '-v',
-    `${__dirname}/../articles/:/data/_posts/:rw`, 'brunokrebs/auth0-blog:latest'
+const { spawn } = require("child_process");
+const ls = spawn("docker", [
+    "run",
+    "-p",
+    "4000:4000",
+    "--name",
+    "blog",
+    "-d",
+    "-v",
+    `${__dirname}/../articles/:/data/_posts/:rw`,
+    "brunokrebs/auth0-blog:latest"
 ]);
 
-ls.stdout.on('data', data => {
+ls.stdout.on("data", data => {
     console.log(`${data}`);
 });
 
-ls.stderr.on('data', data => {
+ls.stderr.on("data", data => {
     console.log(`${data}`);
 });
